26092cf726ff358247405a40847d91dde5154b55
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-07-12 12:06:47 +0500

    updated the recipe detailed screen

--- lib/screens/recipe_details.dart
+++ lib/screens/recipe_details.dart
@@ -7,38 +7,10 @@ class RecipeDetils extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final myrecipe = arguments['recipe'] as String;
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            // title: Row(
-            //   children: [
-            //     IconButton(
-            //       icon: const Icon(Icons.clear, color: Colors.white),
-            //       onPressed: () {
-            //         Navigator.pop(context);
-            //       },
-            //     ),
-               
-            //   ],
-            // ),
-            // bottom: PreferredSize(
-            //   preferredSize:  const Size.fromHeight(40),
-            //   child: Container(
-            //     width: double.maxFinite,
-            //     padding: const EdgeInsets.only(top: 10, bottom: 10),
-            //     decoration: const BoxDecoration(
-            //       color: Colors.white,
-            //       borderRadius: BorderRadius.only(
-            //         topLeft: Radius.circular(15),
-            //         topRight: Radius.circular(15)
-            //       )
-            //     ),
-                
-            //     //child: Center(child: Text(recipe.title, style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
-            //   ),
-            // ),
             pinned: true,
             backgroundColor: Colors.black,
             expandedHeight: 300,
